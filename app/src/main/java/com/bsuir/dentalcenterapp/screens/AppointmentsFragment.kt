de18dfe1c8986e16a65271e2186bd8b40281a1bb
Alexander Shumak <alexander.shumak@itexus.com>
2022-11-22 11:50:23 +0300

    Sort appointments by date

--- app/src/main/java/com/bsuir/dentalcenterapp/screens/AppointmentsFragment.kt
+++ app/src/main/java/com/bsuir/dentalcenterapp/screens/AppointmentsFragment.kt
@@ -7,9 +7,13 @@ import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bsuir.dentalcenterapp.models.Appointment
+import com.bsuir.dentalcenterapp.models.AppointmentResponseData
 import com.bsuir.dentalcenterapp.services.AppointmentsAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.itexus.dentalcenterapp.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AppointmentsFragment : Fragment() {
 
@@ -34,7 +38,62 @@ class AppointmentsFragment : Fragment() {
         super.onResume()
 
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        val appointments = viewModel.getAppointments()
+        val appointments = viewModel.getAppointments().sortByDate()
         recyclerView.adapter = AppointmentsAdapter(appointments)
+    }
+
+    private fun List<AppointmentResponseData>.sortByDate(): List<AppointmentResponseData> {
+        val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        return this.map { it.copy(title = it.title.toDate()) }.sortedByDescending {
+            LocalDate.parse(it.title, dateTimeFormatter)
+        }.map { it.copy(it.title.toStringDate()) }
+    }
+
+    private fun String.toDate(): String {
+        val splittedString = this.split(" ")
+        val day = if (splittedString.first().length == 1) {
+            "0${splittedString.first()}"
+        } else {
+            splittedString.first()
+        }
+        val month = when (splittedString[1]) {
+            "января" -> "01"
+            "февраля" -> "02"
+            "марта" -> "03"
+            "апреля" -> "04"
+            "мая" -> "05"
+            "июня" -> "06"
+            "июля" -> "07"
+            "августа" -> "08"
+            "сентября" -> "09"
+            "октября" -> "10"
+            "ноября" -> "11"
+            else -> "12"
+        }
+        return "$day-$month-2022"
+    }
+
+    private fun String.toStringDate(): String {
+        val splittedString = this.dropLast(5).split("-")
+        val day = if (splittedString.first().first() == '0') {
+            splittedString.first().drop(1)
+        } else {
+            splittedString.first()
+        }
+        val month = when (splittedString[1]) {
+            "01" -> "января"
+            "02" -> "февраля"
+            "03" -> "марта"
+            "04" -> "апреля"
+            "05" -> "мая"
+            "06" -> "июня"
+            "07" -> "июля"
+            "08" -> "августа"
+            "09" -> "сентября"
+            "10" -> "октября"
+            "11" -> "ноября"
+            else -> "декабря"
+        }
+        return "$day $month"
     }
 }
